ad561c690c84ac39477ebdd2b3b5d7919aa0c7d7
ckenken <mikewis11@yahoo.com.tw>
2013-03-24 17:15:46 +0800

    add BMI practice project
    大致上照著課本把 BMI 的初始版本寫好了
    想要看的人可以看看，也可以拿來改成更酷的造型之類的

---
+++ BMI/src/com/example/bmi/Bmi.java
@@ -0,0 +1,57 @@
+package com.example.bmi;
+
+import java.text.DecimalFormat;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Bmi extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button b1 = (Button)findViewById(R.id.submit);
+		b1.setOnClickListener(calcBMI);
+	
+	}
+
+	private OnClickListener calcBMI = new OnClickListener()
+	{
+		public void onClick(View v) 
+		{
+			DecimalFormat nf = new DecimalFormat("0.00");			
+			EditText fh = (EditText)findViewById(R.id.height);
+			EditText fw = (EditText)findViewById(R.id.weight);
+			
+			double height = Double.parseDouble(fh.getText().toString())/100.0;
+			double weight = Double.parseDouble(fw.getText().toString());
+			
+			double BMI = weight/(height * height);
+			
+			TextView result = (TextView)findViewById(R.id.result);
+			result.setText("Your BMI = " + nf.format(BMI));
+			
+			
+		}
+	};
+	
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	
+	
+
+}
