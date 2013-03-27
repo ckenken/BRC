ad4754221294c615eaa27ea1260b2422e14bf992
Eva <blue800415@gmail.com>
2013-03-27 17:04:45 +0800

    add forget_password page
    include link and warning eliminated

--- BRC/BRC/src/com/example/brc/MainActivity.java
+++ BRC/BRC/src/com/example/brc/MainActivity.java
@@ -3,22 +3,41 @@ package com.example.brc;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.widget.*;
-import android.view.*;
+import android.view.*;  
+import android.widget.TextView;
+
+
+
 public class MainActivity extends Activity {
 
-	
+	private TextView login_error;
+	private TextView mail_error;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.index);
     
-    }  
+    }
+    public void login(View view){
+    	login_error = (TextView)findViewById(R.id.password_error); 
+    	login_error.setVisibility(View.VISIBLE);  
+    	  //setContentView(R.layout.home);
+    }
+    public void password_forget(View view){
+    	  setContentView(R.layout.forget);
+    }
+    
+    public void mail_error(View view){
+    	mail_error = (TextView)findViewById(R.id.mail_error); 
+    	mail_error.setVisibility(View.VISIBLE);  
+    	  //setContentView(R.layout.home);
+    }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action  bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
