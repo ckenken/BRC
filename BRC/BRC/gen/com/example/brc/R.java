49eb54e6259cb13c1edf2984a8aa24baff948ef3
Ya-Hung Chen <nizi1127@gmail.com>
2013-04-23 20:41:27 +0800

    modify buttin, when press the button, then the button's color will conver red to pink. and add forget password a title "取回密碼".

--- BRC/BRC/gen/com/example/brc/R.java
+++ BRC/BRC/gen/com/example/brc/R.java
@@ -29,28 +29,30 @@ public final class R {
     public static final class drawable {
         public static final int bg=0x7f020000;
         public static final int bg2=0x7f020001;
-        public static final int button_red=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int logo=0x7f020004;
-        public static final int logo2=0x7f020005;
-        public static final int logo3=0x7f020006;
-        public static final int rounded_edittext=0x7f020007;
+        public static final int button_bg=0x7f020002;
+        public static final int button_pink=0x7f020003;
+        public static final int button_red=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int logo=0x7f020006;
+        public static final int logo2=0x7f020007;
+        public static final int logo3=0x7f020008;
+        public static final int rounded_edittext=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f090012;
-        public static final int button_forget=0x7f090008;
-        public static final int button_login=0x7f090007;
-        public static final int button_register=0x7f090006;
-        public static final int buy_together=0x7f09000c;
+        public static final int button_forget=0x7f090009;
+        public static final int button_login=0x7f090008;
+        public static final int button_register=0x7f090007;
+        public static final int buy_together=0x7f090004;
+        public static final int find_password=0x7f090006;
         public static final int forget_account=0x7f090001;
         public static final int forget_mail=0x7f090000;
         public static final int forget_ok=0x7f090003;
-        public static final int logo=0x7f090004;
         public static final int logoImg=0x7f090005;
-        public static final int logoimg=0x7f09000b;
+        public static final int logoimg=0x7f09000c;
         public static final int mail_error=0x7f090002;
-        public static final int password=0x7f090009;
-        public static final int password_error=0x7f09000a;
+        public static final int password=0x7f09000a;
+        public static final int password_error=0x7f09000b;
         public static final int register_account=0x7f09000e;
         public static final int register_again_password=0x7f09000f;
         public static final int register_mail=0x7f090010;
@@ -71,6 +73,7 @@ public final class R {
         public static final int again_password=0x7f060005;
         public static final int app_name=0x7f060000;
         public static final int buyTogether=0x7f06000e;
+        public static final int find_password=0x7f06000f;
         public static final int hello_world=0x7f060002;
         public static final int login=0x7f060008;
         public static final int logo=0x7f06000d;
