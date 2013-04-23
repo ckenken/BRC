4f79dd833fe3619545902b3ae76ba62ee2b5ad60
Ya-Hung Chen <nizi1127@gmail.com>
2013-04-23 10:45:18 +0800

    modify logo
    modify logo and add it into index layout

--- BRC/BRC/gen/com/example/brc/R.java
+++ BRC/BRC/gen/com/example/brc/R.java
@@ -26,26 +26,28 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
         public static final int logo=0x7f020003;
         public static final int logo2=0x7f020004;
-        public static final int rounded_edittext=0x7f020005;
+        public static final int logo3=0x7f020005;
+        public static final int rounded_edittext=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080010;
+        public static final int action_settings=0x7f080011;
         public static final int button_forget=0x7f080008;
         public static final int button_login=0x7f080007;
         public static final int button_register=0x7f080006;
         public static final int forget_account=0x7f080001;
         public static final int forget_mail=0x7f080000;
         public static final int forget_ok=0x7f080003;
-        public static final int imageView1=0x7f080004;
+        public static final int imageView1=0x7f08000b;
+        public static final int logo=0x7f080004;
+        public static final int logoImg=0x7f080005;
         public static final int mail_error=0x7f080002;
         public static final int password=0x7f080009;
         public static final int password_error=0x7f08000a;
-        public static final int register_account=0x7f08000c;
-        public static final int register_again_password=0x7f08000d;
-        public static final int register_mail=0x7f08000e;
-        public static final int register_ok=0x7f08000f;
-        public static final int register_password=0x7f08000b;
-        public static final int textView1=0x7f080005;
+        public static final int register_account=0x7f08000d;
+        public static final int register_again_password=0x7f08000e;
+        public static final int register_mail=0x7f08000f;
+        public static final int register_ok=0x7f080010;
+        public static final int register_password=0x7f08000c;
     }
     public static final class layout {
         public static final int forget=0x7f030000;
