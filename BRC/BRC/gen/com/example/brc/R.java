4870a64c0dd615ffd4750aa8f561a79f15c1a3ed
Ya-Hung Chen <nizi1127@gmail.com>
2013-05-14 17:59:07 +0800

    add new icon figures

--- BRC/BRC/gen/com/example/brc/R.java
+++ BRC/BRC/gen/com/example/brc/R.java
@@ -33,11 +33,14 @@ public final class R {
         public static final int button_pink=0x7f020003;
         public static final int button_red=0x7f020004;
         public static final int ic_launcher=0x7f020005;
-        public static final int logo=0x7f020006;
-        public static final int logo2=0x7f020007;
-        public static final int logo3=0x7f020008;
-        public static final int more=0x7f020009;
-        public static final int rounded_edittext=0x7f02000a;
+        public static final int ic_message=0x7f020006;
+        public static final int ic_shopping=0x7f020007;
+        public static final int ic_shoppingcar=0x7f020008;
+        public static final int logo=0x7f020009;
+        public static final int logo2=0x7f02000a;
+        public static final int logo3=0x7f02000b;
+        public static final int more=0x7f02000c;
+        public static final int rounded_edittext=0x7f02000d;
     }
     public static final class id {
         public static final int action_settings=0x7f09001b;
